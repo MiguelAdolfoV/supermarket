160a6b54d4b4b3286ca2a62b0899c35443228e9d
Ninis <MiguelAdolfoV@outlook.com>
2024-12-03 00:23:34 -0600

    catalog-service agregado

---
+++ supermarket/src/app/services/catalog.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CatalogService {
+  private apiUrl = 'http://localhost:3000'; // URL del backend
+
+  async getCategories() {
+    const response = await axios.get(`${this.apiUrl}/categories`);
+    return response.data;
+  }
+
+  async getProducts(categoryId: number) {
+    const response = await axios.get(`${this.apiUrl}/products/${categoryId}`);
+    return response.data;
+  }
+  
+
+  updateProductStock(productId: number, quantitySold: number): Promise<any> {
+    return axios.put(`http://localhost:3000/products/${productId}`, {
+      quantitySold,
+    });
+  }
+  
+  
+}
